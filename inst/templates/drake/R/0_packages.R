17d92c51e8847aa135fe0fadfdcaf08b0c92dfab
Eduardo Alfonso-Sierra <edalfon@gmail.com>
2021-03-18 10:24:26 +0100

    clean 0_packages.R a little and modify vignette

--- inst/templates/drake/R/0_packages.R
+++ inst/templates/drake/R/0_packages.R
@@ -1,6 +1,4 @@
 # Load required packages with pacman::p_load() to install them when missing
-# TODO: Consider using xfun::pkg_attach(..., install = TRUE)
-#       I don't like that it uses character vectors, though.
 if(!requireNamespace("pacman", quietly = TRUE)) {
   install.packages("pacman")
 }
@@ -9,12 +7,10 @@ pacman::p_load(
   dplyr,
   tidyr,
   ggplot2,
-  patchwork,
 
   assertr,
 
   conflicted,
-  dotenv,
   drake
 )
 
@@ -22,31 +18,15 @@ conflicted::conflict_prefer("filter", "dplyr", quiet = TRUE)
 conflicted::conflict_prefer("select", "dplyr", quiet = TRUE)
 conflicted::conflict_prefer("expand", "tidyr", quiet = TRUE)
 
-# TODO: how to deal with dependencies (packages that need to be available)
-#       but do not need to be attached/loaded (e.g. we use only one of its fns).
-#       In a package that would go in the DESCRIPTION. Let's see how to do it.
-#       Perhaps just renv::hydrate() once and not here 'cause this runs every
-#       time r_make() and other r_* drake functions run.
-#       Also, renv::hydrate() seems like a heavyweight, so perhaps better
-#       only use DESCRIPTION and remotes::install_deps().
-#       And set the lock file from renv, ja, but without forking library
-
 # Meanwhile, let's use xfun::pkg_load
 deps_pkgs <- c(
   "waldo",
   "qs",
   "sessioninfo",
+  "tictoc",
   "bookdown"
 )
 xfun::pkg_load(deps_pkgs, install = TRUE)
 
-
-# TODO: for projects where we do not want to use renv, let's how to deal
-#       with dependencies
-#       Perhaps pacman::p_install_version()
-#
-# TODO: and let's see how better integrate renv
-
-# Check all packages used in this code
-# renv::dependencies()$Package %>% unique()
-
+# TODO: better articulate with DESCRIPTION and renv
+#       use DESCRIPTION and remotes::install_deps()
