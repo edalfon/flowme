9429df43df87e54d8fb38800bb1c8d55a95c14f5
Eduardo Alfonso-Sierra <edalfon@gmail.com>
2021-03-18 16:41:01 +0100

    remove comments

--- inst/templates/drake/_drake.R
+++ inst/templates/drake/_drake.R
@@ -36,8 +36,4 @@ the_plan <- drake::bind_plans(
 )
 
 # Here just follow dflow, by default changing only format to qs and history=F
-## _drake.R must end with a call to drake_config().
-## The arguments to drake_config() are basically the same as those to make().
-## lock_envir allows functions that alter the random seed to be used. The biggest
-## culprits of this seem to be interactive graphics e.g. plotly and mapdeck.
 drake_config(the_plan, lock_envir = FALSE, format = "qs", history = FALSE)
